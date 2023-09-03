9191b18130211cfc67047ccb2537d66804452021
nancy <smithnancy@outlook.com>
2023-09-03 11:55:42 +1000

    formating the result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,46 @@
 import styled from "@emotion/styled";
 
+const Container = styled.div`
+    color:#fff;
+     font-family: 'Lato', sans-serif;
+     display: flex;
+     align-items: center;
+     gap: 3rem;
+     margin-top: 30px;
+`
+const Text = styled.p`
+   font-size: 18px;
+  span{
+    font-weight: 700;
+  }
+`
+const Price = styled.p`
+   font-size: 24px;
+  span{
+    font-weight: 700;
+  }
+`
+const Image = styled.img`
+  width: 120px;
+  display: block;
+
+`
 
 const Result = ({calculationResult}) => {
     //console.log(calculationResult)
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = calculationResult;
 
   return (
+    <Container>
+      <Image src={`https://cryptocompare.com/${IMAGEURL}`}  alt="Image"/>
       <div>
-          <p>Price: <span>{PRICE}</span></p>
-          <p>Highest Price: <span>{HIGHDAY}</span></p>
-          <p>Lowest Price: <span>{LOWDAY}</span></p>
-          <p>Variation las 24H: <span>{CHANGEPCT24HOUR}</span></p>
-          <p>Last Update: <span>{LASTUPDATE}</span></p>
-    </div>
+        <Price>Price: <span>{PRICE}</span></Price>
+        <Text>Highest Price: <span>{HIGHDAY}</span></Text>
+        <Text>Lowest Price: <span>{LOWDAY}</span></Text>
+        <Text>Variation las 24H: <span>{CHANGEPCT24HOUR}</span></Text>
+        <Text>Last Update: <span>{LASTUPDATE}</span></Text>
+       </div>
+    </Container>
   )
 }
 
